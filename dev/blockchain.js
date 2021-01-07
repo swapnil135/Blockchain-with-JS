db4da753302dd77368e7b18458280ccecaa0aed0
swapnil135 <68809502+swapnil135@users.noreply.github.com>
2021-01-07 10:57:18 +0530

    Add files via upload

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,1 +1,43 @@
+function Blockchain()
+{
+  this.chain=[];
+  this.pendingTransactions=[];
+}
 
+Blockchain.prototype.createNeBlock = function(nonce,previousBlockHash,hash)
+{
+  const newBlock ={
+    index:this.chain.length+1,
+    timestamp:Date.now(),
+    // all of the transactions in this block will be the transactions that waiting to be put in a block
+    transactions:this.pendingTransactions,
+    // nonce is hust a number giving proof of the transaction
+    nonce:nonce,
+    hash:hash,
+    previousBlockHash: previousBlockHash
+  }
+  // As we move all the pending transactions to the new block, we clear this array
+  this.pendingTransactions=[];
+  this.chain.push(newBlock);
+  return newBlock; n
+}
+Blockchain.prototype.getLastBlock=function()
+{
+  return this.chain[this.chain.length-1];
+}
+Blockchain.prototype.createNewTransaction=function(amount,sender,recepeint)
+{
+  const newTransaction={
+    amount:amount,
+    sender: sender,
+    recipient:recepeint};
+  }
+  // push the newly created transaction in the pendingTransactions array
+  // All the transactions in this pendinfTransactions array are not validated, that happens when a new black is created
+  this.pendingTransactions.push(newTransaction);
+
+  // Return the index of the block of which this transaction will be part of
+  return this.getLastBlock()['index']+1;
+}
+
+module.exports=Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,1 +1,7 @@
+const Blockchain = require('./Blockchain');
+const bitcoin = new Blockchain();
 
+bitcoin.createNeBlock(1,'123dsf132f','123f44f4');
+bitcoin.createNeBlock(34,'123dasaASDf132f','123f4ASDAE4f4');
+bitcoin.createNeBlock(13,'123dsf13SFQR','12WERG344f4');
+console.log(bitcoin);
